2c0ce469d7f537cfa2f1019fe00a15a970b76594
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-05-02 22:37:41 -0300

    Subindo layout padrão dos dashboard + pop up de registrar carta

---
+++ js/redirecionamentos.js
@@ -0,0 +1,12 @@
+function redirecionar_home() {
+    window.location.href = "index.html"
+}
+function redirecionar_cadastro() {
+    window.location.href = "Login_Cadastro.html"
+}
+function redirecionar_ferramenta() {
+    location.hash = `#div_secao_ferramenta`
+}
+function redirecionar_aulas() {
+    window.location.href = "https://tcg.pokemon.com/pt-br/top-deck-academy/"
+}
