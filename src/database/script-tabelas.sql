8c2ec6830a605f4bdf64a9abebadb429817dc077
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-05-15 13:10:06 -0300

    Subindo pequenas alterações de qualidade de exibição

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,44 +6,30 @@
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE pokemon_tcg;
 
-USE aquatech;
+USE pokemon_tcg;
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
-	email VARCHAR(50),
+	email VARCHAR(200),
 	senha VARCHAR(50)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE cartas (
+	idCarta INT AUTO_INCREMENT,
+	nome VARCHAR(100),
+	imagemURL VARCHAR(150),
+	tipo VARCHAR(45),
+	raridade VARCHAR(45),
+	idSet VARCHAR(10),
+	fkUsuario INT, 
+	numero VARCHAR(15),
+	CONSTRAINT fkUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(id),
+	CONSTRAINT pkComposta PRIMARY KEY (idCarta, fkUsuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
 
 /*
